774138387b94a23b8b07441e1567b4b61a2c4dee
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-12-24 21:50:05 +0900

    initial

---
+++ codes/testing_arithm.R
@@ -0,0 +1,12 @@
+## this is the start of an R script
+## the heading provides some information about the file
+
+## File name: testing_arithm.R
+## Author: Seunghoon Yang
+## Purpose: Practicing basic math commands and commenting in R
+
+5 - 3
+5 / 3
+5 ^ 3
+5 * (10 - 3)
+sqrt(4)
